dc0f2d33712d15aca79fe7ad07fed5b1c06181ed
iksor <iksorek@gmail.com>
2021-04-14 23:53:18 +0100

    ADD mobile no add form

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('mobile');
+            $table->string('mobile')->nullable();
             $table->timestamp('mobile_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-gray-500 border-b border-gray-200">
+                    @if(!Auth::user()->mobile)
+                        <p class="m-4">There is no mobile number added yet! You have to add it before continue</p>
+                        <div>
+                            <form action="">
+                            <x-label>Mobile number</x-label>
+                        <x-input placeholder="+44" type="text" class="m-5"></x-input>
+                            <x-button>Submit</x-button>
+                            </form>
+                        </div>
+
+
+
+
+                    @elseif(!Auth::user()->mobile_verified_at)
+                        <p>Your mobile number is not verified yet</p>
+                    @endif
                 </div>
+
+
             </div>
         </div>
     </div>
