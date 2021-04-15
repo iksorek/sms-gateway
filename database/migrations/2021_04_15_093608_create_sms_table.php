4e72914355b187399017ecf623ce5baf414fc65f
iksor <iksorek@gmail.com>
2021-04-15 10:41:36 +0100

    ADD Sms model SET relations ADD Sms migration

---
+++ database/migrations/2021_04_15_093608_create_sms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('message');
+            $table->timestamp();
+            $table->string('status');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms');
+    }
+}
